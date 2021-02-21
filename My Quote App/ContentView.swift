51267b4667509620d7a091f5c68fd3c95d011ab9
simplilearn_github <cloude056@yahoo.fr>
2021-02-21 13:10:46 +0200

    My Own Quotes App

--- My Quote App/ContentView.swift
+++ My Quote App/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        VStack {
+        
+            Text("You don't deserve to be hurt,emotionally or physically.").font(.headline).fontWeight(.bold).multilineTextAlignment(.center).padding(.all, 0.0)
+            Spacer()
+            HStack {
+                
+                Image(systemName: "heart.fill")
+                Spacer()
+                Image(systemName: "heart.fill")
+            }
+            
+          
+        }
+        
     }
 }
 
